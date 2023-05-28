2434dcff13ea81c3258c5d0c344a16fbd20cf786
Ajeet Kumar Jha <84560142+Ajeetjha123@users.noreply.github.com>
2023-05-28 12:34:17 +0530

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,73 @@
+function saveInput(event) {
+  event.preventDefault();
+  const formValues = {
+    name: document.getElementById('name').value,
+    email: document.getElementById('email').value,
+    phone: document.getElementById('phone').value,
+  };
+
+  axios.post('https://crudcrud.com/api/your-unique-identifier', formValues)
+    .then(response => {
+      console.log(response.data); // Optional: Display the response data in the console
+      displayFormValues(response.data);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+function displayFormValues(savedValues) {
+  const displayArea = document.createElement('div');
+  const namePara = document.createElement('p');
+  const emailPara = document.createElement('p');
+  const phonePara = document.createElement('p');
+  const deleteIcon = document.createElement('i');
+
+  namePara.textContent = `Name: ${savedValues.name}`;
+  emailPara.textContent = `Email: ${savedValues.email}`;
+  phonePara.textContent = `Phone: ${savedValues.phone}`;
+  deleteIcon.className = 'fas fa-trash-alt'; // Add a font-awesome class for the delete icon
+
+  deleteIcon.addEventListener('click', () => {
+    deleteFormValues(savedValues._id);
+    displayArea.remove();
+  });
+
+  displayArea.appendChild(namePara);
+  displayArea.appendChild(emailPara);
+  displayArea.appendChild(phonePara);
+  displayArea.appendChild(deleteIcon);
+
+  const existingDisplayArea = document.getElementById('displayArea');
+  if (existingDisplayArea) {
+    existingDisplayArea.remove(); // Remove the existing display area if it exists
+  }
+
+  displayArea.id = 'displayArea'; // Set an ID for the display area
+  document.body.appendChild(displayArea);
+}
+
+function deleteFormValues(id) {
+  axios.delete(`https://crudcrud.com/api/your-unique-identifier/${id}`)
+    .then(response => {
+      console.log(response.data); // Optional: Display the response data in the console
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+window.onload = function() {
+  axios.get('https://crudcrud.com/api/your-unique-identifier')
+    .then(response => {
+      const data = response.data;
+      if (data) {
+        data.forEach(savedValues => {
+          displayFormValues(savedValues);
+        });
+      }
+    })
+    .catch(error => {
+      console.error(error);
+    });
+};
